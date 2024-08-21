4c95f320ff152a2877cc467bf8922dc6e093b5a5
Gabriella Massaro <gmassaro807@gmail.com>
2024-08-21 09:27:11 -0400

    Edit ingredient, create ingredient

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,7 +1,18 @@
 class IngredientsController < ApplicationController
+  before_action :set_ingredient, only: [:show, :edit, :update]
+
   def create
-    ingredient = Ingredient.create
-    redirect_to ingredient.recipe
+    @ingredient = Ingredient.new(ingredient_params)
+
+    if @ingredient.save
+      redirect_to @ingredient, notice: 'Ingredient was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def new
+    @ingredient = Ingredient.new
   end
 
   def index
@@ -10,5 +21,30 @@ class IngredientsController < ApplicationController
 
   def show
     @ingredient = Ingredient.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @ingredient.update(ingredient_params)
+        format.html { redirect_to @ingredient, notice: 'Ingredient was successfully updated.' }
+        format.json { render :show, status: :ok, location: @ingredient }
+      else
+        format.html { render :edit }
+        format.json { render json: @ingredient.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def set_ingredient
+    @ingredient = Ingredient.find(params[:id])
+  end
+
+  def ingredient_params
+    params.require(:ingredient).permit(:name, :recipe_id)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,10 +14,17 @@ ActiveRecord::Schema.define(version: 20240820193825) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string   "name"
-    t.integer  "recipe_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["recipe_id"], name: "index_ingredients_on_recipe_id"
+  end
+
+  create_table "recipe_ingredients", force: :cascade do |t|
+    t.integer  "recipe_id"
+    t.integer  "ingredient_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.index ["ingredient_id"], name: "index_recipe_ingredients_on_ingredient_id"
+    t.index ["recipe_id"], name: "index_recipe_ingredients_on_recipe_id"
   end
 
   create_table "recipes", force: :cascade do |t|
